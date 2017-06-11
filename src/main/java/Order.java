2d8fc4ff4c399afa0467a03b75ee718a92cf68f8
JGSxiaofeng <liu_feng000@126.com>
2017-06-11 20:54:00 +0800

    我是小风，开发了订单模块

---
+++ src/main/java/Order.java
@@ -0,0 +1,9 @@
+/**
+ * Created by xiaofeng on 2017/6/11.
+ */
+public class Order {
+    private String orderName;
+    public void say(){
+        System.out.println("这里是订单系统...");
+    }
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Nothing is impossible!");
+        System.out.println("我是小风风！");
     }
+
 }
