e5c9ee25333ac2f373dc3e5b46fbc38ff98997a4
JGSxiaofeng <liu_feng000@126.com>
2017-06-11 20:54:26 +0800

    我是小阳，开发了账户模块

---
+++ src/main/java/Account.java
@@ -0,0 +1,11 @@
+/**
+ * Created by xiaofeng on 2017/6/11.
+ */
+public class Account {
+    private String name;
+    private String accountNo;
+
+    public void say(){
+        System.out.println("这里是账户系统...");
+    }
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Nothing is impossible!");
+        System.out.println("我是小阳阳...");
     }
 }
